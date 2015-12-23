b1d6392fcd3472851104c91c938aa0353aed9078
oimosan <zushitomomi@FL1-122-135-150-60.tky.mesh.ad.jp>
2015-12-23 12:46:59 +0900

    app commit

---
+++ TestApp/TestApp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by 図師ともみ on 2015/12/23.
+//  Copyright © 2015年 FBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ TestApp/TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by 図師ともみ on 2015/12/23.
+//  Copyright © 2015年 FBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
